983a0879cb8d6e166960e207cbfbdad31bffa64a
AdvaitDhingra <62119114+AdvaitDhingra@users.noreply.github.com>
2021-02-23 21:41:50 +0100

    Fixes

--- ElectronWave/electrons.c
+++ ElectronWave/electrons.c
@@ -7,21 +7,29 @@
 #include <math.h>
 
 
+
 float pi = TMath::Pi();
 
 float L = 100; // m
 
 int nx, ny, n;
 
+double x[100], y[100], z[11];
+
+
 float roundval(float var)
 {
-    float value = (int)(var*100+.5);
-    return (float)value/100;
+    float value = (int)(var*1000+.5);
+    return (float)value/1000;
 }
 float waveFunction2D(float energyLevelx, float energyLevely, float lengthx, float lengthy)
 {
-    float wave = (2/L) * ( roundval(sin(energyLevelx * (lengthx/L) *pi)) )*( roundval(sin(energyLevely * (lengthy/L) *pi)));
-    return wave;
+
+    	float wave = ((roundval (sin(energyLevelx * (lengthx/L) *pi))) )*(roundval (sin(energyLevely * (lengthy/L) *pi)));
+    	
+    	return wave;
+    	
+    	
 
 }
 float Amplitude2D(float wave)
@@ -35,34 +43,43 @@ void electrons()
 {
     cout << "Enter a value for n in x direction:" << endl;
     cin >> nx;
-    cout << " Enter a value for n in y direction:" << endl;
+    cout << "Enter a value for n in y direction:" << endl;
     cin >> ny;
-
+    
+	
+	TGraph *two = new TGraph();
+	
     TGraph2D *WaveFunctionTwo = new TGraph2D();
     TGraph2D *AmpTwo = new TGraph2D();
-
-    int count = 0;
-    for (int j = 0; j <= L; j++ )
+	
+	TGraph2D *spare = new TGraph2D();
+	
+	
+	int count = 0;
+    for (int j = 0; j <= L+1; j++ )
     {
-        for (int k = 0; k <= L; k++)
+        for (int k = 0; k <= L+1; k++)
         {
-            WaveFunctionTwo->SetPoint(count, j, k, waveFunction2D(nx, ny, j, k));
-            AmpTwo->SetPoint(count, j, k, Amplitude2D(waveFunction2D(nx, ny, j, k)));
-            count++;
-
+        		float wave = waveFunction2D(nx, ny, j, k);
+            	//WaveFunctionTwo->SetPoint(count, j, k, wave);
+            	WaveFunctionTwo->SetPoint(count, j, k, wave);
+            	AmpTwo->SetPoint(count, j, k, wave*wave);
+            	if (j == 0 || k == 0 || j == L || k == L)
+            		cout << waveFunction2D(nx, ny, 0, 1) << endl;
+            	count++;
 
         }
     }
     TCanvas *c1 = new TCanvas();
-    gStyle->SetPalette(1);
     WaveFunctionTwo->SetTitle("Wavefunction");
-    WaveFunctionTwo->Draw("surf1");
+    WaveFunctionTwo->SetNpx(150);
+    WaveFunctionTwo->SetNpy(150);
+    WaveFunctionTwo->Draw("AP");
     TCanvas *c2 = new TCanvas();
-    gStyle->SetPalette(1);
-    AmpTwo->SetTitle("Amplitude");
     AmpTwo->GetXaxis()->SetTitle("x");
     AmpTwo->GetYaxis()->SetTitle("y");
     AmpTwo->Draw("surf1");
+    
     c1->Print("wavefunction.png");
     c2->Print("amplitude.png");
 }
