ba6d753b20f863431bb5a14d14aa52e42a09cb49
AdvaitDhingra <advait.dhingra1006@gmail.com>
2021-02-23 16:35:55 +0100

    Fixed rounding error

--- ElectronWave/electrons.c
+++ ElectronWave/electrons.c
@@ -3,34 +3,24 @@
 // Display the wavefunction and amplitude of an electron in a box
 //
 
-float hBar = 4.135667696e-15;
+#include <TMath.h>
+#include <math.h>
 
-float pi = 3.141592653589;
 
-float m = 510998.95; // MeV/c
+float pi = TMath::Pi();
 
 float L = 100; // m
 
-float a = 1000;
-
-float b = 1000;
-
 int nx, ny, n;
 
-float wavefunction(float energyLevel, float length)
+float roundval(float var)
 {
-    float wave = sqrt(2/L) * sin(energyLevel*pi/L * length);
-
-    return wave;
-}
-float amplitude(float wave)
-{
-    return wave * wave;
+    float value = (int)(var*100+.5);
+    return (float)value/100;
 }
 float waveFunction2D(float energyLevelx, float energyLevely, float lengthx, float lengthy)
 {
-    float wave = 2/L * sin(energyLevelx * pi * lengthx/L) * sin(energyLevely*pi*lengthy/L);
-
+    float wave = (2/L) * ( roundval(sin(energyLevelx * (lengthx/L) *pi)) )*( roundval(sin(energyLevely * (lengthy/L) *pi)));
     return wave;
 
 }
@@ -45,52 +35,34 @@ void electrons()
 {
     cout << "Enter a value for n in x direction:" << endl;
     cin >> nx;
-    cout << " Enter a value forn in y direction:" << endl;
+    cout << " Enter a value for n in y direction:" << endl;
     cin >> ny;
 
-    //TH1F *values = new TH1F("Values", "Values", 100, 0, L);
-
-    //double xs[1000], ys[1000];
-
-    //TGraph2D *WaveFunction2D = new TGraph();
-    //TGraph2D *Amplitude2D = new TGraph();
     TGraph2D *WaveFunctionTwo = new TGraph2D();
     TGraph2D *AmpTwo = new TGraph2D();
 
-    TGraph* WaveFunction = new TGraph();
-    TGraph* Amplitude = new TGraph();
-
-    //for (int i = 0; i < L; i++)
-    //{
-      //  WaveFunction->SetPoint(WaveFunction->GetN(), i, wavefunction(1, i));
-      //  Amplitude->SetPoint(Amplitude->GetN(), i, amplitude(wavefunction(1, i)));
-
-    //}
     int count = 0;
-    for (int j = 0; j < L; j++ )
+    for (int j = 0; j <= L; j++ )
     {
-        for (int k = 0; k < L; k++)
+        for (int k = 0; k <= L; k++)
         {
             WaveFunctionTwo->SetPoint(count, j, k, waveFunction2D(nx, ny, j, k));
             AmpTwo->SetPoint(count, j, k, Amplitude2D(waveFunction2D(nx, ny, j, k)));
             count++;
+
+
         }
     }
-    //TCanvas *c1 = new TCanvas();
-    //WaveFunction->SetTitle("Wave Function");
-    //WaveFunction->Draw();
-    //TCanvas *c2 = new TCanvas();
-    //Amplitude->SetTitle("Amplitude");
-    //Amplitude->Draw();
-    TCanvas *c3 = new TCanvas();
+    TCanvas *c1 = new TCanvas();
     gStyle->SetPalette(1);
     WaveFunctionTwo->SetTitle("Wavefunction");
     WaveFunctionTwo->Draw("surf1");
-    TCanvas *c4 = new TCanvas();
+    TCanvas *c2 = new TCanvas();
     gStyle->SetPalette(1);
     AmpTwo->SetTitle("Amplitude");
+    AmpTwo->GetXaxis()->SetTitle("x");
+    AmpTwo->GetYaxis()->SetTitle("y");
     AmpTwo->Draw("surf1");
-
-    c3->Print("wavefunction.png");
-    c4->Print("amplitude.png");
+    c1->Print("wavefunction.png");
+    c2->Print("amplitude.png");
 }
